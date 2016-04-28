66d134509ff4e73bfd04c1c6bca564312f79c290
davidswindell <davidswindell2@msn.com>
2016-04-28 17:34:07 +0100

    added twipp

--- twitter/1.1/account/verify_credentials.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * account/verify_credentials
- * https://dev.twitter.com/docs/api/1.1/get/account/verify_credentials
- */
- 
-require '../../twitter-proxy.php';
-
-Proxy::relay( 'account/verify_credentials' );
